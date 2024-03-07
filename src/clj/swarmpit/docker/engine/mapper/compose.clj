e15600a47c6983903bbcd5eece46c08e9d9ea484
Constantine Volodin <constantine.a.v@gmail.com>
2024-03-08 02:42:09 +0300

    cap

--- src/clj/swarmpit/docker/engine/mapper/compose.clj
+++ src/clj/swarmpit/docker/engine/mapper/compose.clj
@@ -72,8 +72,8 @@
                              {:interval (str (:interval healthcheck) "s")}
                              {:timeout (str (:timeout healthcheck) "s")})))
      :tty (-> service :tty)
-     :cap_add
-     :cap_drop
+     :cap_add (-> service :cap_add)
+     :cap_drop (-> service :cap_drop)
      :environment (-> service :variables (name-value->sorted-map))
      :ports (->> service :ports
                  (map #(str (:hostPort %) ":" (:containerPort %) (when (= "udp" (:protocol %)) "/udp"))))
